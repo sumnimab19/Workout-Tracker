7c4bd4dc10c04d5798868e90f0456199243aaf5f
Sumnima B <sumnimabhandari@gmail.com>
2020-07-31 22:33:02 -0700

    Created Schema in  workout.js based on seed.js

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [{
+        type:{
+            type: String,
+            trim: true,
+            required: "Enter the type of workout." 
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter name of the workout." 
+        },
+
+        duration: {
+            type: Number,
+            required: "Enter the workout duration." 
+        },
+
+        weight: {
+            type: Number,
+            required: "Enter the weight." 
+        },
+
+        sets: {
+            type: Number,
+            required: "Enter the number of sets." 
+        },
+
+        reps: {
+            type: Number,
+            required: "Enter the number of reps." 
+        },
+
+        distance: {
+            type: Number,
+            required: "Enter the distance." 
+        }
+
+    }]
+});
+
+const Workout = mongoose.model("Workout",workoutSchema);
+
+module.exports = Workout;
